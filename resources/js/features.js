e8b8e59303890cabee457e82adab15634f3ed00d
hexu <hexu.garfield@gmail.com>
2020-09-21 22:43:25 +0800

    Add more custom components

--- resources/js/features.js
+++ resources/js/features.js
@@ -10,4 +10,11 @@ Nova.booting((Vue) => {
     Vue.component('custom-detail-header', comp('detail-header'))
     Vue.component('custom-index-toolbar', comp('index-toolbar'))
     Vue.component('custom-index-header', comp('index-header'))
+
+    Vue.component('custom-attach-header', comp('attach-header'))
+    Vue.component('custom-create-header', comp('create-header'))
+    Vue.component('custom-dashboard-header', comp('dashboard-header'))
+    Vue.component('custom-lens-header', comp('lens-header'))
+    Vue.component('custom-update-header', comp('update-header'))
+    Vue.component('custom-update-attached-header', comp('update-attached-header'))
 })
